1698ac3848814749cdff0d578f5e035a869b9734
Elias Breitenbach <elias.b@me.com>
2023-03-20 16:07:42 +0100

    Pushed the project to github

---
+++ Key_points/Key_points/Key_pointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_pointsApp.swift
+//  Key_points
+//
+//  Created by Elias Breitenbach on 20.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Key_pointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Key_points/Key_pointsUITests/Key_pointsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Key_pointsUITestsLaunchTests.swift
+//  Key_pointsUITests
+//
+//  Created by Elias Breitenbach on 20.03.23.
+//
+
+import XCTest
+
+final class Key_pointsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
